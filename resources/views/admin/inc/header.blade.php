43555ccc977567b357522691e70e323fdb8542fb
Alihan Öztürk <alihanozturk364@gmail.com>
2024-02-20 21:47:15 +0300

    header completed

--- resources/views/admin/inc/header.blade.php
+++ resources/views/admin/inc/header.blade.php
@@ -29,15 +29,13 @@
             <div class="d-none d-sm-block">
                 <div class="dropdown pt-3 d-inline-block">
                     <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Create <i class="mdi mdi-chevron-down"></i>
+                            {{ __('common.button.create') }} <i class="mdi mdi-chevron-down"></i>
                         </a>
 
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Separated link</a>
+                        <a class="dropdown-item" href="#">{{ __('common.button.item') }}</a>
+                        <a class="dropdown-item" href="#">{{ __('common.button.item') }}</a>
+                        <a class="dropdown-item" href="#">{{ __('common.button.item') }}</a>
                     </div>
                 </div>
             </div>
@@ -77,7 +75,7 @@
                     <div class="p-3">
                         <div class="row align-items-center">
                             <div class="col">
-                                <h5 class="m-0 font-size-16"> Notifications (258) </h5>
+                                <h5 class="m-0 font-size-16"> {{ __('common.title.notifications') }} (258) </h5>
                             </div>
                         </div>
                     </div>
@@ -175,7 +173,7 @@
                     <div class="p-2 border-top">
                         <div class="d-grid">
                             <a class="btn btn-sm btn-link font-size-14 text-center" href="javascript:void(0)">
-                                View all
+                                {{ __('common.button.view_all') }}
                             </a>
                         </div>
                     </div>
@@ -185,17 +183,16 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="/assets/images/users/user-4.jpg"
-                        alt="Header Avatar">
+                    <i class="fas fa-user-circle" style="font-size:36px"></i>
+                   
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle font-size-17 align-middle me-1"></i> Profile</a>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-wallet font-size-17 align-middle me-1"></i> My Wallet</a>
-                    <a class="dropdown-item d-flex align-items-center" href="#"><i class="mdi mdi-cog font-size-17 align-middle me-1"></i> Settings<span class="badge bg-success ms-auto">11</span></a>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline font-size-17 align-middle me-1"></i> Lock screen</a>
+                    <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle font-size-17 align-middle me-1"></i> {{ __('common.title.profile') }}</a>
+                    <a class="dropdown-item d-flex align-items-center" href="#"><i class="mdi mdi-cog font-size-17 align-middle me-1"></i> {{ __('common.title.preferences') }}</a>
+                    <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline font-size-17 align-middle me-1"></i> {{ __('common.title.lock_screen') }}</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="#"><i class="bx bx-power-off font-size-17 align-middle me-1 text-danger"></i> Logout</a>
+                    <a class="dropdown-item text-danger" href="#"><i class="bx bx-power-off font-size-17 align-middle me-1 text-danger"></i> {{ __('common.title.logout') }}</a>
                 </div>
             </div>
 
